306c314699b05b55031a3263fae6a9d01c2290e4
Henry Li <henry1943@163.com>
2021-08-30 16:12:11 +0800

    chore: add pages build

---
+++ scripts/webpack/webpack.pages.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const common = require('./webpack.common.config');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    path: path.resolve(__dirname, '../../docs'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../../docs/index.html'),
+      template: path.resolve(__dirname, '../../src/index.html'),
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        removeComments: true,
+        removeRedundantAttributes: true,
+        removeScriptTypeAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        useShortDoctype: true,
+        minifyCSS: true,
+        minifyJS: true,
+      },
+    }),
+  ],
+});

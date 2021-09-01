2e89b7bfe0ab6a8a25d8624295d954631d28c1fd
Henry Li <henry1943@163.com>
2021-09-01 11:30:50 +0800

    chore: use airbnb instead of creat-react-app

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,29 @@
 module.exports = {
-  extends: ['react-app', 'prettier'],
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/jsx-runtime',
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.json',
+  },
   rules: {
-    'prefer-const': 'warn',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-empty-interface': 'off',
+    'arrow-body-style': 'off',
+    'class-methods-use-this': 'off',
+    'import/extensions': 'off',
+    'import/prefer-default-export': 'off',
+    'no-else-return': 'off',
+    'no-param-reassign': 'off',
+    'no-plusplus': 'off',
+    'no-underscore-dangle': 'off',
+    'no-use-before-define': 'off',
   },
 };
